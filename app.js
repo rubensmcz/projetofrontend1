73487c5520ea39701c544ac817638acb71b2293a
cd-199 <cd-199>
2017-12-20 21:04:03 -0300

    projeto final

--- app.js
+++ app.js
@@ -10,15 +10,59 @@
 
 function getPosts(URI) {
    $.getJSON(URI, function(data) {
-   	console.log ('getPosts: ' + data.length) ;
-   	//console.log(data.length);
-   });
+   	let items = [];
+      $.each( data, function(key, val) {
+         items.push(postDom(key,val));
+      });   
+
+      
+
+      $( "<ul/>", {
+         "class": "Artigo_Pai",
+         html: items.join("")
+       }).appendTo(".Post_List");
+
+      });
+
+
+}
+
+
+function postDom(key, value) {
+   let now = Date();
+   let post = '<li class="Artigo_Filho">' + 
+              '<h3>' + value.title          + '</h3>' + 
+              '<p>' + value.body           + '</p>'  +
+              '<span>'                    + now + '</span>'
+              '</li>';
+
+            return post
 }
 
 
 function getAuthors(URI) {
    $.getJSON(URI, function(data) {
-   	console.log ('getAuthors: ' + data.length);
+   	//console.log ('getAuthors: ' + data.length);
    	//console.log(data.length);
+      let items = [];
+      let el = document.querySelector('.Autores_Pai');
+
+      for(let contador in data) {
+         items.push(autorDom(data[contador]))
+      }
+      console.log(items);
+      el.innerHTML = items.join("");
+
    });
+}
+
+function autorDom(item) {
+     let autor = '<li class="Autores_Filho">' +
+                 '<div class = "bola" >' + '</div>' +
+                 '<div class = "Autores_lista">' +
+                    '<span>' + item.username + '</span>' + '<br>' +
+                    '<span>' + item.email + '</span>' +
+                  '</div>' +                          
+                 '</li>';
+      return autor;
 }
